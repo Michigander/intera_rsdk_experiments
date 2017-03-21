caa1930ba55b9116996874815d7641796e0044f0
Gordon Finnie <gordon.m.finnie.iii@gmail.com>
2017-03-21 07:21:55 -0400

    changes to interface and examples.

--- exp_examples/head_display_camera.py
+++ exp_examples/head_display_camera.py
@@ -1,36 +1,48 @@
-import argparse 
-OBimport numpy as np 
+import argparse
+OBimport numpy as np
 
-import cv2 
-import cv_bridge import CvBridge, CvBridgeError
+import cv2
+import cv_bridge
+import CvBridge, CvBridgeError
 
-import rospy 
-import intera_interface 
+import rospy
+import intera_interface
+import xp_interface
+import SimpleHeadDisplay
 
-def route_image (img_data, (edge_detection, window_name)):
+def process_image (img_msg, (edge_detection, window_name)):
     """ Callback to display camera image on head. """
-    bridge = CvBridge()
+
     try:
-        cv_image = bridge.imgmsg_to_cv2(img_data, "bgr8")
+        cv_image = CvBridge.imgmsg_to_cv2(img_msg, "bgr8")
+
     except CvBridgeError, err:
         rospy.logerr(err)
         return
+
     if edge_detection == True:
         gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-        # customize the second and the third argument, minVal and maxVal                                
-        # in function cv2.Canny if needed                                                               
+        # customize the second and the third argument, minVal and maxVal
+        # in function cv2.Canny if needed
         get_edge = cv2.Canny(blurred, 10, 100)
         cv_image = np.hstack([get_edge])
-        
-    
-def main (): 
-    """ Head display with camera data experimental example """
+
+    SimpleHeadDisplaysend_image(cv_image)
+
+def main ():
+    """
+    Head display with camera data experimental example
+    """
+
+    # ensure cam is valid
     rp = intera_interface.RobotParams()
-    valid_cameras = rp.get_camera_names() 
-    if not valid_cameras: 
+    valid_cameras = rp.get_camera_names()
+    if not valid_cameras:
         rp.log_message(("Cannot detect any camera_config parameters on this robot. Exiting."), "ERROR")
-        return 
+        return
+
+    # parse arguments
     arg_fmt = argparse.RawDescriptionHelpFormatter
     parser = argparse.ArgumentParser(formatter_class=arg_fmt, description=main.__doc__)
     parser.add_argument(
@@ -42,23 +54,31 @@ def main ():
     parser.add_argument(
         '-e', '--edge', action='store_true',
         help='Streaming the Canny edge detection image')
-    args = parser.parse_args() 
-    
-    print("Initializing camera display node ...") 
+    args = parser.parse_args()
+
+    # initialize nodes
+    print("Initializing camera display node ...")
     rospy.init_node('camera_display', anonymous=True)
-    camera = intera_interface.Cameras() 
+
+    print("Initializing head display node ...")
+    rospy.init_node('head_display', anonymous=True)
+
+    # start streaming
+    camera = intera_interface.Cameras()
     if not camera.verify_camera_exists(args.camera):
         rospy.logerr("Invalid camera name, exiting experiment")
-        return 
-    camera.start_streaming(args.camera) 
+        return
+    camera.start_streaming(args.camera)
     rectify_image = not args.raw
-    use_canny_edge = args.edge 
-    camera.set_callback(args.camera, show_image_callback,
-                        rectify_image=rectify_image, callback_args=(use_canny_edge, args.camera)) 
-    
+    use_canny_edge = args.edge
+    camera.set_callback(args.camera,
+                        route_image,
+                        rectify_image=rectify_image,
+                        callback_args=(use_canny_edge, args.camera
+                        ))
+
     rospy.loginfo("Streaming. Ctrl-C to quit")
     rospy.spin()
 
-    
-if __name__ == '__main__': 
-    main() 
+if __name__ == '__main__':
+    main()
--- exp_interface/simple_head_display.py
+++ exp_interface/simple_head_display.py
@@ -1,48 +1,23 @@
 File Edit Options Buffers Tools Python Help                                                            import rospy
 
 from sensor_msgs.msg import Image
-
+import cv_bridge
 
 class SimpleHeadDisplay(object):
-    
+
     def __init__(self):
-        """                                                                                             
-        Constructor                                                                                     
+        """
+        Constructor
         """
         self._image_pub = rospy.Publisher('/robot/head_display', Image, latch=True, queue_size=10)
 
 
-    def display_image(self, img_msg):
-        """                                                                                             
-        Displays image(s) to robot's head                                                               
-                                                                                                        
-        @type image_path: list                                                                          
-        @param image_path: the relative or absolute file path to the image file(s)                      
-                                                                                                        
-        @type display_in_loop: bool                                                                     
-        @param display_in_loop: Set True to loop through all image files infinitely                     
-                                                                                                        
-        @type display_rate: float                                                                       
-        @param display_rate: the rate to publish image into head                                        
+    def display_image(self, img):
         """
-        rospy.logdebug("Display images in loop:'{0}', frequency: '{1}'".format(display_in_loop, display\
-_rate))
-
-        image_msg = []
-        image_list = image_path if isinstance(image_path, list) else [image_path]
-        for one_path in image_list:
-            cv_img = self._setup_image(one_path)
-            if cv_img:
-                image_msg.append(cv_img)
-
+        Publish a single image message
+        """
         if not image_msg:
-            rospy.logerr("Image message list is empty")
-        else:
-            r = rospy.Rate(display_rate)
-            while not rospy.is_shutdown():
-                for one_msg in image_msg:
-                    self._image_pub.publish(one_msg)
-                    r.sleep()
-                if not display_in_loop:
-                    break
-
+            rospy.logerr("Image message has no data!")
+        else if not :
+            cv_bridge.CvBridge().cv2_to_imgmsg(img, encoding="bgr8")
+            self._image_pub.publish(img_msg)
